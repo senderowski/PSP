30d6e68fbeb9b60ac68cfeb0fdf68b6f5129cfe2
mawatech.pl <senderowski1984@gmail.com>
2025-07-21 09:06:10 +0200

    Create sw.js

---
+++ psp_test/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('rcp-cache').then(cache => {
+      return cache.addAll(['index.html', 'manifest.json']);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
